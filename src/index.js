d724937947b717d57cd2c7d2401acc4e6a46b43e
LucasFarias <mi.ohta5@gmail.com>
2021-10-05 08:46:55 -0300

    ok ok

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import MonacoEditor from 'react-monaco-editor'
 
-
+const code =
+`
+// Define Typescript Interface Employee
+interface Employee {
+    firstName: String;
+    lastName: String;
+    contractor?: Boolean;
+}
+// Use Typescript Interface Employee. 
+// This should show you an error on john 
+// as required attribute lastName is missing
+const john:Employee = {
+    firstName:"John",
+    // lastName:"Smith"
+    // contractor:true
+}
+`
 
 function App() {
+
+  // constructor(props){
+  //   super(props)
+  //   this.state = { code }
+  // }
+
+onChange(newValue, e) {
+    //  console.log('onChange', newValue, e);
+}
+
+
     return (
-      <h1> hello world </h1>
+      <MonacoEditor
+      width="600"
+      height="800"
+      language="javascript"
+      theme="vs-dark"
+      defaultValue='//'
+      value={code}
+      onChange={this.onChange}/>
     )
 }
 
